e60f6f9edae3dc8145ee20b4d24af0cd42b124fe
Will Comfort <comfortwb21@gmail.com>
2019-08-26 21:10:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  dif = num1 - num2
+  p dif
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  p quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  p product
 end
 
 def modulo(num1, num2)
